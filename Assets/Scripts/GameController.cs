f94f142e3ead91930cb06f01eef87b2df6ce12c7
Siddharth Tiwari <sidh589@gmail.com>
2021-10-10 22:48:19 +0530

    Created Code for Generating Cards.
    Created a script for cardData for creation of Scriptable objects.
    
    Added Different Scriptable Objects for different Cards, such as:-
    * FireAttackCard
    *Fire2AttackCard
    *IceAttackCard
    *Ice2AttackCard
    *FireandIceCard
    *Health1Card
    *Health2Card
    *MirrorCard;
    
    Then created a Card Script to keep different data for cards and attached to card object in Hierarchy.
    
    Created arrays named DamageNumbers and HealthNumbers as they were sprites and needed to be referenced as numbers when required.
    
    Also assigned each sprites with their respective numbers in arrays.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    static public GameController instance = null;
+
+    public Sprite[] healthNumbers = new Sprite[10];
+    public Sprite[] damageNumbers = new Sprite[10];
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
     public void Quit()
     {
         SceneManager.LoadScene(0);
